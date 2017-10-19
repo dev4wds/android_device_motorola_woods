71512a00c5461ef97c3aa5606b7a3d17f7f76c0e
zac6ix <nanaquameallday@gmail.com>
2017-10-19 17:12:07 +0000

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,10 +1,10 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := E4
+PRODUCT_RELEASE_NAME := woods
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/woods/device_woods.mk)
@@ -12,6 +12,10 @@ $(call inherit-product, device/motorola/woods/device_woods.mk)
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 DEVICE_RESOLUTION := 720x1280
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := woods

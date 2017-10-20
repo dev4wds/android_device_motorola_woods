bbd3f870c2b35f44b43f1508aeaf08acdd852bc4
zac6ix <nanaquameallday@gmail.com>
2017-10-20 10:11:37 +0000

    switch to validus

--- validus.mk
+++ validus.mk
@@ -4,7 +4,7 @@
 PRODUCT_RELEASE_NAME := woods
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/woods/device_woods.mk)
@@ -19,7 +19,7 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := woods
-PRODUCT_NAME := lineage_woods
+PRODUCT_NAME := validus_woods
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto E4
 PRODUCT_MANUFACTURER := Motorola

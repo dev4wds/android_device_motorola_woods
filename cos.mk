96ec7a6a173642838e81f3f11cbebe9a858bb75f
zac6ix <nanaquameallday@gmail.com>
2017-10-23 03:07:16 +0000

    switch to cos

--- cos.mk
+++ cos.mk
@@ -4,7 +4,7 @@
 PRODUCT_RELEASE_NAME := woods
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/aicp/configs/common.mk)
+$(call inherit-product, vendor/cos/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/woods/device_woods.mk)
@@ -19,7 +19,7 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := woods
-PRODUCT_NAME := aicp_woods
+PRODUCT_NAME := cos_woods
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto E4
 PRODUCT_MANUFACTURER := Motorola

54b1a55e0e97fce6c5213fd4242d88599f1b41f3
zac6ix <nanaquameallday@gmail.com>
2017-10-23 03:03:51 +0000

    Delete common.mk

--- vendor/dev4wds/common.mk
+++
@@ -1,6 +0,0 @@
-# Common
-DEVICE_PACKAGE_PATH += device/motorola/woods/vendor/dev4wds
-
-# Bootanimation
-PRODUCT_COPY_FILES += \
-$(DEVICE_PACKAGE_PATH)/bootanimation/bootanimation.zip:system/media/bootanimation.zip

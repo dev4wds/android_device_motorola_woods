43661c92b0dcfe0ed60fdec54f02fab526a648d7
zac6ix <nanaquameallday@gmail.com>
2017-11-05 09:33:16 +0000

    add Portuguese (Brazil) language | test | dot-os

--- dot.mk
+++ dot.mk
@@ -3,8 +3,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := woods
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/woods/device_woods.mk)
@@ -19,7 +19,7 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := woods
-PRODUCT_NAME := lineage_woods
+PRODUCT_NAME := dot_woods
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto E4
 PRODUCT_MANUFACTURER := Motorola
@@ -27,4 +27,4 @@ PRODUCT_MANUFACTURER := Motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 # Available languages
-PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG
+PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG pt-BR

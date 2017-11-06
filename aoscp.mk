7ad2dc296dcf8517b710ed202368334c1234607b
zac6ix <nanaquameallday@gmail.com>
2017-11-06 23:00:30 +0000

    bring aoscp to woods

--- aoscp.mk
+++ aoscp.mk
@@ -3,8 +3,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := woods
 
-# Inherit some common DotOS stuff.
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+# Inherit some common aoscp stuff.
+$(call inherit-product, vendor/aoscp/configs/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/woods/device_woods.mk)
@@ -19,7 +19,7 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := woods
-PRODUCT_NAME := dot_woods
+PRODUCT_NAME := aoscp_woods
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto E4
 PRODUCT_MANUFACTURER := Motorola
@@ -29,6 +29,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 # Available languages
 PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG pt_BR
 
-#use magisk
-export WITH_MAGISK=true
-ROOT_METHOD=magisk

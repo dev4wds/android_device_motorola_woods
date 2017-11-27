b24dd4a072c4e268f3a62b5bc7d315057d22953a
zac6ix <nanaquameallday@gmail.com>
2017-11-27 21:51:37 +0000

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ../../../..
 cd frameworks/av
+<<<<<<< HEAD
 git apply -v ../../device/motorola/woods/patches/0001-frameworks_av.patch
 cd ../..
 cd frameworks/base
@@ -14,4 +15,19 @@ git apply -v ../../device/motorola/woods/patches/0004-system_netd.patch
 cd ../..
 cd system/core
 git apply -v ../../device/motorola/woods/patches/0005-system_core.patch
+=======
+git apply -v ../../device/moto/woods/patches/0001-frameworks_av.patch
+cd ../..
+cd frameworks/base
+git apply -v ../../device/moto/woods/patches/0002-frameworks_base.patch
+cd ../..
+cd frameworks/native
+git apply -v ../../device/moto/woods/patches/0003-frameworks_native.patch
+cd ../..
+cd system/netd
+git apply -v ../../device/moto/woods/patches/0004-system_netd.patch
+cd ../..
+cd system/core
+git apply -v ../../device/moto/woods/patches/0005-system_core.patch
+>>>>>>> a4feb89... Update apply-patches.sh
 cd ../..

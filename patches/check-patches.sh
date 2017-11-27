355e2c2323fc1e8fca33b52bc7b8c66fff908af3
zac6ix <nanaquameallday@gmail.com>
2017-11-27 21:52:08 +0000

    Update check-patches.sh

--- patches/check-patches.sh
+++ patches/check-patches.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ../../../..
 cd frameworks/av
+<<<<<<< HEAD
 git apply -v --check ../../device/motorola/woods/patches/0001-frameworks_av.patch
 cd ../..
 cd frameworks/base
@@ -14,4 +15,19 @@ git apply -v --check ../../device/motorola/woods/patches/0004-system_netd.patch
 cd ../..
 cd system/core
 git apply -v --check ../../device/motorola/woods/patches/0005-system_core.patch
+=======
+git apply -v --check ../../device/moto/woods/patches/0001-frameworks_av.patch
+cd ../..
+cd frameworks/base
+git apply -v --check ../../device/moto/woods/patches/0002-frameworks_base.patch
+cd ../..
+cd frameworks/native
+git apply -v --check ../../device/moto/woods/patches/0003-frameworks_native.patch
+cd ../..
+cd system/netd
+git apply -v --check ../../device/moto/woods/patches/0004-system_netd.patch
+cd ../..
+cd system/core
+git apply -v --check ../../device/moto/woods/patches/0005-system_core.patch
+>>>>>>> 9fbf740... Update check-patches.sh
 cd ../..

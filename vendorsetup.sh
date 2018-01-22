815a0de4b32438fc9fd924dcf14192a2ef534906
Izaq Kull <izaqkull@yahoo.com>
2018-01-22 16:24:18 +0000

    auto patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,22 @@
-# Use this to add P1m to CM's lunch command menu
-for var in eng user userdebug; do
-  add_lunch_combo lineage_woods-$var
+for var in userdebug; do
+ add_lunch_combo lineage_woods-$var
 done
+
+# Patches
+cd frameworks/av
+git apply -v ../../device/motorola/woods/patches/0001-frameworks_av.patch
+cd ../..
+cd frameworks/base
+git apply -v ../../device/motorola/woods/patches/0002-frameworks_base.patch
+cd ../..
+cd system/netd
+git apply -v ../../device/motorola/woods/patches/0004-system_netd.patch
+cd ../..
+cd system/core
+git apply -v ../../device/motorola/woods/patches/0005-system_core.patch
+cd ../..
+cd frameworks/native
+git apply -v ../../device/motorola/woods/patches/0003-frameworks_native.patch
+cd ../..
+
+echo " PATCH DONE !!!"

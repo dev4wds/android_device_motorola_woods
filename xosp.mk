12deae84108463a4e29c7b12599717dccf361c95
iykequame <izaqkull@yahoo.com>
2018-01-31 01:52:16 +0000

    specify root method

---
+++ xosp.mk
@@ -0,0 +1,35 @@
+## Specify phone tech before including full_phone
+
+# Release name
+PRODUCT_RELEASE_NAME := woods
+
+# Inherit some common XOSP stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/configs/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/woods/device_woods.mk)
+
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+DEVICE_RESOLUTION := 720x1280
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := woods
+PRODUCT_NAME := xosp_woods
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto E4
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Available languages
+PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG pt_BR
+
+#specify root method
+#ROOT_METHOD := supersu
+ROOT_METHOD := magisk
